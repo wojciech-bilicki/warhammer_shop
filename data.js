08cb68ecf79da89b856915bb2db68c9bc0c76f53
wojtek <wojciech.bilicki@gmail.com>
2019-03-27 19:19:32 +0100

    Init

---
+++ data.js
@@ -0,0 +1,32 @@
+const data = [{
+  id: 1,
+  name: "Space Marine",
+  description: "The Space Marines or Adeptus Astartes are foremost amongst the defenders of humanity, the greatest of the Emperor of Mankind's warriors. They are barely human at all, but superhuman; having been made superior in all respects to a normal man by a harsh regime of genetic modification, psycho-conditioning and rigorous training.",
+  race: "Mankind",
+  type: "Infantry",
+  components: 13,
+  price: 22.50,
+  image: "images/image1.jpg"
+}, 
+{
+  id: 2,
+  name: "Falcon",
+  description: "The Falcon is the main battle tank of the Eldar, a fast, mobile and well-protected grav-tank carrying excellent anti-tank and anti-infantry firepower. In addition to the Falcon's considerable assets, it can also transport a small squad of Aspect Warriors or Eldar Guardians into battle, and its split wing design and single seat turret are a familiar sight to those enemy tank crews that face the Eldar on the battlefield.",
+  race: "Eldar",
+  type: "Tank",
+  components: 9,
+  price: 43.25,
+  image: "images/image2.jpg"
+}, 
+{
+  id: 3,
+  name: "Imotekh",
+  description: "Imotekh, also known as Imotekh the Stormlord or simply the Stormlord, is the Phaeron of the Sautekh Dynasty and perhaps the strongest and most accomplished Necron Overlord in the galaxy at the end of the 41st Millennium.",
+  race: "Necron",
+  type: "Hero",
+  components: 20,
+  price: 30.00,
+  image: "images/image3.jpg"
+}]
+
+export default data;
